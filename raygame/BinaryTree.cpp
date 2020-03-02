7bd2960a51e3da0bf4395130b5a927ac2748d9c2
s198061 <s198061@LAF-7E868C>
2020-03-02 17:55:52 -0600

    added more funtions

--- raygame/BinaryTree.cpp
+++ raygame/BinaryTree.cpp
@@ -4,8 +4,8 @@
 
 BinaryTree::BinaryTree()
 {
+	m_pRoot = nullptr;
 }
-
 
 BinaryTree::~BinaryTree()
 {
@@ -13,29 +13,98 @@ BinaryTree::~BinaryTree()
 
 bool BinaryTree::isEmpty() const
 {
+	if (m_pRoot == nullptr)
+	{
+		return true;
+	}
 	return false;
 }
 
 void BinaryTree::insert(int a_nValue)
 {
+	//If the tree is empty, the value is inserted at the root
+	if (isEmpty)
+	{
+		m_pRoot->getData = a_nValue;
+	}
+
+	//Set the current node to the root
+	TreeNode* cNode = m_pRoot;
+
+	//While the current node is not null
+	while (cNode != nullptr)
+	{
+		if (a_nValue < cNode->getData)
+		{
+			cNode->setLeft;
+			continue;
+		}
+		if (a_nValue > cNode->getData)
+		{
+			cNode->setRight;
+			continue;
+		}
+		if (a_nValue = cNode->getData)
+		{
+			break;
+		}
+	}
+
+	
+
+	if ()
+	{
+
+	}
 }
 
 void BinaryTree::remove(int a_nValue)
 {
+
+
+	if ()
+	{
+
+	}
+
+	if ()
+	{
+
+	}
 }
 
 TreeNode * BinaryTree::find(int a_nValue)
 {
+
 	return nullptr;
 }
 
 void BinaryTree::draw(TreeNode * selected)
 {
+	//Draw root node
 	draw(m_pRoot, 640, 680, 640, selected);
 }
 
 bool BinaryTree::findNode(int a_nSearchValue, TreeNode ** ppOutNode, TreeNode ** ppOutParent)
 {
+	TreeNode* cNode = m_pRoot;
+
+	while (cNode != nullptr)
+	{
+		if ()
+		{
+
+		}
+		if ()
+		{
+
+		}
+	}
+
+	if ()
+	{
+
+	}
 	return false;
 }
 
